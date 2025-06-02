efa8e0dc6e0d97338d92b53e9b1cbbc5b121d85b
Kevin Hernandez <kevin.alfonso.hernandez.amaya@gmail.com>
2025-06-02 14:09:38 -0700

    Input System y Input Manager | Practica Modulo 12
    Se modifica los scripts Bola y Jugador para recibir movimiento y acción de disparo de bola.

--- Assets/_Scripts/Bola.cs
+++ Assets/_Scripts/Bola.cs
@@ -1,16 +1,29 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Bola : MonoBehaviour
 {
+    public bool isGameStarted = false; // Indica si el juego ha comenzado
+    public float velocidadBola = 25f; // Velocidad de la bola
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // Posicionar la bola en el centro de la pantalla del paddle
+        Vector3 posicionInicial = GameObject.FindGameObjectWithTag("Jugador").transform.position;
+        posicionInicial.y += 3; // Ajustar la posición Y para que esté encima del paddle
+        this.transform.position = posicionInicial; // Asignar la posición inicial a la bola
+        this.transform.SetParent(GameObject.FindGameObjectWithTag("Jugador").transform); // Hacer que la bola sea hija del jugador
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Keyboard.current.spaceKey.isPressed || Input.GetButton("Submit"))
+        {
+            isGameStarted = true; // Cambiar el estado del juego a iniciado
+            this.transform.SetParent(null); // Desvincular la bola del jugador
+            GetComponent<Rigidbody>().linearVelocity = velocidadBola * Vector3.up; // Aplicar una velocidad inicial hacia arriba
+        }
     }
 }
--- Assets/_Scripts/Jugador.cs
+++ Assets/_Scripts/Jugador.cs
@@ -1,16 +1,63 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Jugador : MonoBehaviour
 {
+    public int limiteX = 23; // Límite en el eje X para el jugador
+    public float velocidadPaddle = 45f; // Velocidad de movimiento del jugador
+
+
+    Vector3 mousePos2D;
+    Vector3 mousePos3D;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //mousePos2D = Input.mousePosition; //Input Manager Legacy
+
+        float movimientoTeclado = 0f; // Inicializar el movimiento del teclado en 0
+
+        // Controlar con el teclado y Control de xbox o PS4
+        transform.Translate(Input.GetAxis("Horizontal") * Vector3.down * velocidadPaddle * Time.deltaTime); // Mover el jugador en el eje X según la entrada horizontal (Input Manager Legacy) (Controlar con un control de Xbox o PS4)
+
+        Vector3 pos = this.transform.position; // Obtener la posición actual del jugador
+
+        // Controlar con las flechas del teclado con el nuevo Input System (comentado porque ya se controla con el Input Manager Legacy)
+        //if (Keyboard.current.leftArrowKey.isPressed) // Si se presiona la flecha izquierda
+        //{
+        //    //transform.Translate(Vector3.up * velocidadPaddle * Time.deltaTime); // Mover a la izquierda
+        //    movimientoTeclado = -1f; // Asignar un valor negativo para mover a la izquierda
+        //}
+        //else if (Keyboard.current.rightArrowKey.isPressed) // Si se presiona la flecha derecha
+        //{
+        //    //transform.Translate(Vector3.down * velocidadPaddle * Time.deltaTime); // Mover a la derecha
+        //    movimientoTeclado = 1f; // Asignar un valor positivo para mover a la derecha
+        //}
+
+        //if (movimientoTeclado != 0f)
+        //{
+        //    //pos.x += movimientoTeclado * velocidadPaddle * Time.deltaTime; // Actualizar la posición X del jugador según el movimiento del teclado
+        //}
+
+        // Controlar con el mouse
+        if (movimientoTeclado == 0f && Input.GetAxis("Horizontal") == 0)
+        {
+            // Usar el nuevo Input System para obtener la posición del mouse
+            mousePos2D = Mouse.current.position.ReadValue(); // Obtener la posición del mouse en 2D
+            mousePos2D.z = -Camera.main.transform.position.z; // Asignar la posición Z de la cámara para convertir a 3D
+            mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D); // Convertir la posición 2D del mouse a 3D
+            pos.x = mousePos3D.x; // Actualizar la posición X del jugador con la posición X del mouse
+        }
+
+        // Limitar la posición del jugador dentro de los límites establecidos
+        if (pos.x < -limiteX) pos.x = -limiteX; // Limitar la posición X a la izquierda
+        else if (pos.x > limiteX) pos.x = limiteX; // Limitar la posición X a la derecha
+        this.transform.position = pos; // Asignar la nueva posición al jugador
     }
 }
